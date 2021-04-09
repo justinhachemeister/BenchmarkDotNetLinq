9796ee2abbf6e12a7b2599e1664183c54d513cd2
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2021-04-09 15:42:22 +0100

    add project with some benchmarks #1 #2 #3

---
+++ BenchmarkDotNetLinq/BenchmarkLinq.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNetLinq
+{
+    [MemoryDiagnoser]
+    public class BenchmarkLinq
+    {
+        private List<Drink> _drinks;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _drinks = new Fixture()
+                .CreateMany<Drink>(10000)
+                .ToList();
+        }
+
+        [Benchmark]
+        public int AlcoholicDrinksCountLinqWhere()
+        {
+            return _drinks.Where(x => x.IsAlcoholic).Count();
+        }
+
+        [Benchmark]
+        public int AlcoholicDrinksCountLinqCount()
+        {
+            return _drinks.Count(x => x.IsAlcoholic);
+        }
+
+        [Benchmark]
+        public int AlcoholicDrinksCountForLoop()
+        {
+            var count = 0;
+            for (var i = 0; i < _drinks.Count; i++)
+            {
+                if (_drinks[i].IsAlcoholic)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        [Benchmark]
+        public int AlcoholicDrinksCountForEachLoop()
+        {
+            var count = 0;
+            foreach (var t in _drinks)
+            {
+                if (t.IsAlcoholic)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ BenchmarkDotNetLinq/Drink.cs
@@ -0,0 +1,15 @@
+﻿namespace BenchmarkDotNetLinq
+{
+    public class Drink
+    {
+        /// <summary>
+        /// Name of the drink
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Tru if is alcoholic and false otherwise
+        /// </summary>
+        public bool IsAlcoholic { get; set; }
+    }
+}
